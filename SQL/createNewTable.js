7a0923f4b36ba706f52b57e6ed2df73d7f603c60
Marta <martatricoli42@gmail.com>
2024-03-20 17:48:52 +0100

    sql exercise: create new table

---
+++ SQL/createNewTable.js
@@ -0,0 +1,22 @@
+import pgPromise from "pg-promise";
+
+const db = pgPromise()("postgress://postgres:postgres@localhost:5432/postgres");
+
+const setUpDb = async () => {
+  await db.none(`
+    CREATE TABLE books(
+      book_id INTEGER PRIMARY KEY,
+      title TEXT NOT NULL,
+      author TEXT NOT NULL,
+      genre TEXT NOT NULL,
+      published_year TEXT NOT NULL,
+      isbn TEXT NOT NULL,
+      price TEXT NOT NULL,
+      rating TEXT NOT NULL,
+      stock_count TEXT NOT NULL,
+    )
+  `)  
+}
+setUpDb();
+
+export { db };
