bc57978c056570fedafc2d0fbecac49a87611eda
Marta <martatricoli42@gmail.com>
2024-03-22 15:59:45 +0100

    add commit.js

---
+++ SQL/commit.js
@@ -0,0 +1,14 @@
+import pgPromise from "pg-promise";
+
+const db = pgPromise()("postgress://postgres:postgres@localhost:5432/postgres");
+
+const setUpDb = async () => {
+  await db.none(`
+    BEGIN TRASACTION;
+    DELETE FROM books WHERE id = 101;
+    COMMIT;
+  `)  
+}
+setUpDb();
+
+export { db };
